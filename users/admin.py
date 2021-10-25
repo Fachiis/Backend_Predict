7b5138d0142f86814921c1b02f10a4e361c5baaf
Fachii Zasha <zashafachii@gmail.com>
2021-10-25 22:24:59 +0100

    First Commit of the Project This is the first commit of the project having defined the user model and default settings configuration

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from users.forms import CustomUserCreationForm, CustomUserChangeForm
+from users.models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    list_display = ['username', 'email']
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
