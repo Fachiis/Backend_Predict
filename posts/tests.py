7e43b6fc40f5a197979121ae50c04ef50d900ebe
Fachii Zasha <zashafachii@gmail.com>
2021-10-26 23:08:57 +0100

    Add Tests and f string format
    Add test for the Post Model in the posts/tests.py
    Reformat the str method in CustomUser Model to use the f string format
    Add comments in the users/tests.py for reason why test is not carried out.

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,33 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+from posts.models import Post
+
+User = get_user_model()
+
+
+class PostTest(TestCase):
+
+    def setUp(self):
+        """The set up test data for the post test using class method instead of object methods"""
+        # Create a
+        self.test_user = User.objects.create_user(
+            username="fachiis",
+            password="1234"
+        )
+
+        # Create a post
+        test_post = Post.objects.create(
+            author=self.test_user,
+            title="Grace",
+            body="Yes, it is by His Grace alone",
+        )
+
+    def test_post_content(self):
+        post = Post.objects.get(id=1)
+        author = f'{post.author}'
+        title = f'{post.title}'
+        body = f'{post.body}'
+        self.assertEqual(author, "fachiis")
+        self.assertEqual(title, "Grace")
+        self.assertEqual(body, "Yes, it is by His Grace alone")
