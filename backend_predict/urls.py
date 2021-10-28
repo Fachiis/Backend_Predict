58add6e1fa0f6f425e0a1b4fecd500f03fb94800
Fachii Zasha <zashafachii@gmail.com>
2021-10-28 13:26:14 +0100

    Configuration for restframework added to settings.py Adds path for apis app, django rest auth and restframework documentation routers Adds likes field which is a many to many field to Post model Adds value field which is a charfield to Like model Adds test for posts apps Update the requirements.txt file up-to-date

--- backend_predict/urls.py
+++ backend_predict/urls.py
@@ -14,8 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from allauth.account.views import confirm_email
+from rest_framework_swagger.views import get_swagger_view
+from rest_framework.documentation import include_docs_urls
+
+API_TITLE = 'Post API'
+swagger_view = get_swagger_view(title=API_TITLE)
 
 urlpatterns = [
+    # Django Admin Router
     path('admin/', admin.site.urls),
+
+    # Local App Router
+    path('api/v1/', include('apis.urls')),
+
+    # Django REST Router
+    path('api-auth/', include('rest_framework.urls')),
+
+    # Django Rest Auth Router
+    path('api/v1/rest-auth/', include('rest_auth.urls')),
+
+    # Django Rest Auth Registration Router
+    path('api/v1/rest-auth/registration/', include('rest_auth.registration.urls')),
+
+    # Django account confirm email router with key generated
+    re_path(r"^rest-auth/registration/account-confirm-email/(?P<key>[\s\d\w().+-_',:&]+)/$", confirm_email,
+            name="account_confirm_email"),
+
+    # Django REST Framework documentation Router
+    path('api/docs/', swagger_view),
 ]
